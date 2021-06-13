d725dc50ec677dfe409d88a3ffea6dce8effad62
zelo2 <55820816+zelo2@users.noreply.github.com>
2021-06-13 16:48:24 +0800

    Add notations by Milix

--- EduCDM/DINA/DINA.py
+++ EduCDM/DINA/DINA.py
@@ -6,13 +6,14 @@ import numpy as np
 from tqdm import tqdm
 import pickle
 from EduCDM import CDM
+from itertools import product
 
 
 def initial_all_knowledge_state(know_num):
     state_num = 2 ** know_num
     all_states = np.zeros((state_num, know_num))
     for i in range(state_num):
-        k, quotient, residue = 1, i // 2, i % 2
+        k, quotient, residue = 1, i // 2, i % 2  # quotient:商 residue:余数
         while True:
             all_states[i, know_num - k] = residue
             if quotient <= 0:
@@ -20,6 +21,12 @@ def initial_all_knowledge_state(know_num):
             quotient, residue = quotient // 2, quotient % 2
             k += 1
     return all_states
+
+def _get_all_skills(know_num):
+    # 获得所有可能被试技能的排列组合
+    size = know_num
+    # 取【0，1】的笛卡尔积，repeat指定iterable重复几次
+    return np.array(list(product([0, 1], repeat=size)))
 
 
 def init_parameters(stu_num, prob_num):
@@ -45,31 +52,54 @@ class DINA(CDM):
         self.stu_num, self.prob_num, self.know_num = stu_num, prob_num, know_num
         self.theta, self.slip, self.guess = init_parameters(stu_num, prob_num)
         self.all_states = initial_all_knowledge_state(know_num)  # shape = (state_num, know_num)
+        # keepdims:保持其二维性
+        # np.sum(q, axis=1) sum each row. Size = question * 1
+        # Transpose-> 1 * question
+        # 1 * question - question * (2**k) = (2**k) * question
+        # state_prob里大于0的元素表示无法答对该试题，具体数字表示缺少的掌握知识点个数。
         state_prob = np.transpose(np.sum(q_m, axis=1, keepdims=True) - np.dot(q_m, np.transpose(self.all_states)))
         self.eta = 1 - (state_prob > 0)  # state covers knowledge of problem (1: yes), shape = (state_num, prob_num)
 
     def train(self, epoch, epsilon) -> ...:
         like = np.zeros(shape=(self.stu_num, self.state_num))  # likelihood
-        post = np.zeros(shape=(self.stu_num, self.state_num))  # posterior
+        post = np.zeros(shape=(self.stu_num, self.state_num))  # posterior 后验
         theta, slip, guess, tmp_R = np.copy(self.theta), np.copy(self.slip), np.copy(self.guess), np.copy(self.R)
         tmp_R[np.where(self.R == self.skip_value)[0], np.where(self.R == self.skip_value)[1]] = 0
         for iteration in range(epoch):
             post_tmp, slip_tmp, guess_tmp = np.copy(post), np.copy(slip), np.copy(guess)
-            answer_right = (1 - slip) * self.eta + guess * (1 - self.eta)
-            for s in range(self.state_num):
+            answer_right = (1 - slip) * self.eta + guess * (1 - self.eta)  # (2**k) * question
+            for s in range(self.state_num):  # O(2**k) Loop
+                # question * (stu * question) = stu * question
                 log_like = np.log(answer_right[s, :] + 1e-9) * self.R + np.log(1 - answer_right[s, :] + 1e-9) * (
                     1 - self.R)
+                # except the unknown data
                 log_like[np.where(self.R == self.skip_value)[0], np.where(self.R == self.skip_value)[1]] = 0
+                # sum each row. Shape = stu_num
                 like[:, s] = np.exp(np.sum(log_like, axis=1))
+            # compute the posterior. Like's shape = stu * (2**k)
+            # 这里假设所有认知状态的存在概率是相同的，即先验概率相同，因此计算后验时可以约掉所有的先验概率，
             post = like / np.sum(like, axis=1, keepdims=True)
+
+            # np.sum(post, axis=0) sum each column. Shape = 2 ** k
+            # post's Shape = (2**k, None)
+            # np.expand_dims(post, axis=1)表示在1位置添加数据
+            # (2 ** k, None) expand -> (2**k) * 1
             i_l = np.expand_dims(np.sum(post, axis=0), axis=1)  # shape = (state_num, 1)
+
+            # ((2**k) * stu) dot (stu * question) = (2**k) * question
+            # sum(每个认知状态对每个学生对应的后验概率 * 每个学生在同一试题上的得分) = r_jl(j=question,l=cognitive status)
             r_jl = np.dot(np.transpose(post), tmp_R)  # shape = (state_num, prob_num)
+
+            # eta's shape = (2**k) * question
+            # ((2**k) * question) * ((2**k) * question) = ((2**k) * question)
+            # sum(axis=0) -> shape = question_num
             r_jl_0, r_jl_1 = np.sum(r_jl * (1 - self.eta), axis=0), np.sum(r_jl * self.eta, axis=0)
             i_jl_0, i_jl_1 = np.sum(i_l * (1 - self.eta), axis=0), np.sum(i_l * self.eta, axis=0)
             guess, slip = r_jl_0 / i_jl_0, (i_jl_1 - r_jl_1) / i_jl_1
 
             change = max(np.max(np.abs(post - post_tmp)), np.max(np.abs(slip - slip_tmp)),
                          np.max(np.abs(guess - guess_tmp)))
+            # shape = stu_num, find student's cognitive status
             theta = np.argmax(post, axis=1)
             if iteration > 20 and change < epsilon:
                 break
@@ -109,3 +139,4 @@ class DINA(CDM):
         max_like_id = np.argmax(np.exp(np.sum(log_like, axis=1)))
         dia_state = self.all_states[max_like_id]
         return max_like_id, dia_state
+
